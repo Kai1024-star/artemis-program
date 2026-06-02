137d0b17fd71d7a714cfe9c71ce99d727d6871a9
Kai Zhang <Kai1024-star@users.noreply.github.com>
2026-06-02 11:17:10 +0800

    Remove unsupported pip option

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install "numpy==1.26.4"
-PIP_NO_BUILD_ISOLATION=1 python -m pip install --no-build-isolation --no-use-pep517 "aeneas==1.7.3.0"
+python -m pip install --no-build-isolation "aeneas==1.7.3.0"
 python -m pip install -r requirements.txt
